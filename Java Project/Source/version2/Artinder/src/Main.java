867c69dbf301bb73668494595bad8bb6bfdde488
Liormayn <liormayn@gmail.com>
2015-04-24 14:40:29 +0300

    Adding weka library to project and a code sample

---
+++ Java Project/Source/version2/Artinder/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+/**
+ * Created by Lior on 08/04/2015.
+ */
+public class Main {
+
+public static void main (String[] args) throws Exception {
+    // if not logged in
+
+    String[] arr = new String[] {"C:\\Temp\\artinder"};
+    TextClassifier txt = new TextClassifier();
+    txt.main(arr);
+
+    if (true) {
+        MainPage page = new MainPage();
+    } else
+    {
+        //show article page
+    }
+    }
+}
