22e502f314c5fa3dc51da1932c53a4fb6054e355
Liormayn <liormayn@gmail.com>
2015-06-08 22:27:45 +0300

    V11

---
+++ Java Project/Source/Lior/v11/DB FIXES/Artinder/src/main/java/ArticleFinder.java
@@ -0,0 +1,197 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by Lior on 25/04/2015.
+ */
+public class ArticleFinder {
+
+    private final static String MainFolder = "c:/temp/Artinder";
+    public static List<Article> Unclassified;
+    public static List<Article>  Positive;
+    public static List<Article> Negative;
+    private static int processDoneCounter;
+
+    public static List<Article> GetMatchingArticles(String username) throws Exception,FileNotFoundException {
+       if (Unclassified == null || Positive == null || Negative == null )  DBConnection.GetAllUnclassifiedArticlesForUser(username);
+        processDoneCounter = 0;
+
+
+        //Create the main folder for classified
+        File dir = new File(MainFolder + "/classified");
+         dir.mkdirs();
+
+        //Runnable NegativeTask = () -> WriteNegative();
+
+        //Runnable PositiveTask =() -> WritePositive();
+        //Runnable UnclassifiedTask = () -> WriteUnclassified();
+
+       // Callable<Void> callable = new Callable<Void>() {
+          //  public Void call() {
+         //       // do something
+              //  return null;
+         //   }
+        //Thread WriteNegativeThread = new Thread(NegativeTask);
+        //Thread WritePositiveThread = new Thread(PositiveTask);
+       // Thread WriteUnclassifiedThread = new Thread(UnclassifiedTask);
+
+       // WriteNegativeThread.start();
+       // WritePositiveThread.start();
+       // WriteUnclassifiedThread.start();
+        //Create folder inside for negative
+        //insert negative articles each in text document - title is id and inside - content.
+
+
+        dir = new File(MainFolder + "/classified/Negative");
+        dir.mkdirs();
+        for (Article article : Negative)
+        {
+            PrintWriter writer = new PrintWriter(MainFolder + "/classified/Negative/"+ article.id);
+            writer.println(article.id);
+            writer.println(article.text);
+            writer.close();
+        }
+
+        //Create folder inside for positive
+        //insert positive articles each in text document - title is id and inside- content.
+
+
+        dir = new File(MainFolder + "/classified/Positive");
+        dir.mkdirs();
+        for (Article article : Positive)
+        {
+            PrintWriter writer = new PrintWriter(MainFolder + "/classified/Positive/"+ article.id + ".txt");
+            writer.println(article.id);
+            writer.println(article.text);
+            writer.close();
+        }
+
+
+        //Create different folder for unclassified articles
+        //Create inside folder - "ToClassify"
+        //insert each unclassified article in text document - title is id and inside - content.
+
+
+        dir = new File(MainFolder  + "/classify/unclassified");
+        dir.mkdirs();
+
+
+        for (Article article : Unclassified)
+        {
+            PrintWriter writer = new PrintWriter(MainFolder  + "/classify/unclassified/"+ article.id);
+            writer.println(article.id);
+            writer.println(article.text);
+            writer.close();
+        }
+
+
+       // while (processDoneCounter < 3) {}
+
+        //StartClassification algorithm with folders
+        //Get a list of id's that are classified as positive.
+        String[] arr = new String[] {MainFolder  + "/classified",MainFolder  + "/classify"};
+        TextClassifier txt = new TextClassifier();
+        List<Integer> articleIdsFound = txt.main(arr);
+
+        // get the chosen articles
+            List<Article> chosenArticles = new ArrayList<Article>();
+
+            for ( Article item : Unclassified)
+            {
+                if (articleIdsFound.contains(item.id))
+                {
+                    chosenArticles.add(item);
+                }
+            }
+
+
+        // if list is empty / contains less than 2 , add one or two of the unclassified articles to the list.
+        int randomArticleIndex = 0;
+        while  (chosenArticles.size() < 2)
+        {
+            chosenArticles.add(Unclassified.get(randomArticleIndex));
+            randomArticleIndex++;
+            System.out.println("Negative classified added because there were not enough positive");
+        }
+
+        //delete temporary folders.
+        deleteDirectory(new File(MainFolder));
+
+        return chosenArticles;
+
+    }
+
+    static public boolean deleteDirectory(File path) {
+        if( path.exists() ) {
+            File[] files = path.listFiles();
+            for(int i=0; i<files.length; i++) {
+                if(files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                }
+                else {
+                    files[i].delete();
+                }
+            }
+        }
+        return( path.delete() );
+    }
+
+    static public void WriteNegative()  {
+        try {
+            File dir = new File(MainFolder + "/classified/Negative");
+            dir.mkdirs();
+            for (Article article : Negative) {
+                PrintWriter writer = new PrintWriter(MainFolder + "/classified/Negative/" + article.id);
+                writer.println(article.id);
+                writer.println(article.text);
+                writer.close();
+            }
+            processDoneCounter++;
+        }
+        catch (FileNotFoundException e )
+        {
+
+        }
+    }
+
+    static public void WritePositive() {
+       try
+       {File dir = new File(MainFolder + "/classified/Positive");
+        dir.mkdirs();
+        for (Article article : Positive) {
+            PrintWriter writer = new PrintWriter(MainFolder + "/classified/Positive/" + article.id + ".txt");
+            writer.println(article.id);
+            writer.println(article.text);
+            writer.close();
+        }
+        processDoneCounter++;
+    }
+        catch (FileNotFoundException e)
+        {
+
+        }
+    }
+
+    public static void WriteUnclassified() {
+        try {
+            File dir = new File(MainFolder + "/classify/unclassified");
+            dir.mkdirs();
+            for (Article article : Unclassified) {
+                PrintWriter writer = new PrintWriter(MainFolder + "/classify/unclassified/" + article.id);
+                writer.println(article.id);
+                writer.println(article.text);
+                writer.close();
+            }
+            processDoneCounter++;
+        }
+        catch (FileNotFoundException e)
+        {
+
+        }
+    }
+}
---
+++ Java Project/Source/Lior/v11/DB FIXES/Artinder/src/main/java/Main.java
@@ -0,0 +1,103 @@
+import javax.swing.*;
+import javax.swing.UIManager.*;
+import javax.swing.plaf.metal.MetalRootPaneUI;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.*;
+import java.sql.SQLException;
+
+/**
+ * Created by Lior on 08/04/2015.
+ */
+
+public class Main {
+
+    public static int ClassificationThreshold = 2;
+    public static String userConnected;
+
+    public class AdvancedNimbusLookAndFeel extends NimbusLookAndFeel {
+
+        @Override
+        public boolean getSupportsWindowDecorations() {
+            return true;
+        }
+
+    }
+
+    // a class that extends thread that is to be called when program is exiting
+    static class TerminateProgram extends Thread {
+
+        public void run() {
+            try {
+                DBConnection.CloseConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            Runtime.getRuntime().addShutdownHook(new TerminateProgram());
+            UIManager.installLookAndFeel("AdvancedNimbus", AdvancedNimbusLookAndFeel.class.getName());
+            //UIManager.put("Button.background", Color.CYAN);
+            //UIManager.put("Button.foreground", Color.black);
+            //UIManager.put("Panel.background", Color.gray);
+            //UIManager.put("Button.foreground", Color.black);
+            //UIManager.put("TextArea.border", Color.BLACK);
+            //UIManager.put("TextArea.foreground", Color.white);
+            //UIManager.put("TextArea.background", Color.gray);
+            //UIManager.put("Label.background", Color.DARK_GRAY);
+            //UIManager.put("Text.background", Color.white);
+            UIManager.put("defaultFont", new Font("Ariel", Font.BOLD, 14));
+
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {// --------> Doesn't change nothing!
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            //////////////////////////
+            // if not logged in
+
+            //String[] arr = new String[] {"C:\\Temp\\FinalProject\\artinder","C:\\Temp\\FinalProject\\Classify"};
+            //TextClassifier txt = new TextClassifier();
+            //txt.main(arr);
+
+
+            // DBConnection wk = new DBConnection();
+
+            // wk.RegisterUser("hello","hello");
+            // wk.GetAllArticles();
+            // if (wk.CanUserConnect("lior","lior"))
+            // {
+            //     System.out.println("connected");
+            // }
+
+            // if (wk.CanUserConnect("lior","sdfs"))
+            // {
+            //     System.out.println("not conncted");
+            // }
+
+            //  if (true) {
+            MainPage page = new MainPage();
+        } finally {
+            TerminateProgram terminate = new TerminateProgram();
+            terminate.run();
+        }
+        //  } else
+        // {
+        //show article page
+        //}
+    }
+
+    public static void shutdown()
+    {
+
+    }
+}
